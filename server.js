0f0fe7aa1f57e99e24cf78554de24a1a81373891
Matt Risby <frizzauk@gmail.com>
2013-11-09 07:43:48 +0000

    setup

--- server.js
+++ server.js
@@ -2,25 +2,17 @@
 require('nko')('eKl5JXv67hEA6IFK');
 
 var isProduction = (process.env.NODE_ENV === 'production');
-var http = require('http');
 var port = (isProduction ? 80 : 8000);
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/stemmed-cells" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+var express = require('express');
+var app = express();
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
-  if (err) { console.error(err); process.exit(-1); }
-
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
-    });
-  }
-
-  console.log('Server running at http://0.0.0.0:' + port + '/');
+app.get('/test', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
 });
+
+app.listen(port);
+console.log('Listening on port ' + port);
